4eb448d59727c08b228ba920d36a4ee6dc4c774b
Adrian <66633552+Adrian-Capbatut@users.noreply.github.com>
2023-08-18 20:10:01 +0300

    Button click tracking

--- src/components/btnDarkMode/BtnDarkMode.js
+++ src/components/btnDarkMode/BtnDarkMode.js
@@ -4,8 +4,13 @@ import Moon from "./Moon.svg";
 import "./style.css"
 
 const BtnDarkMode = () => {
+
+    const toggleDarkMode = () => {
+        console.log('toggleDarkMode Fired')
+    }
+
     return (
-        <button className="dark-mode-btn">
+        <button className="dark-mode-btn" onClick={toggleDarkMode}>
             <img src={sun} alt="Light mode" className="dark-mode__icon" />
             <img src={Moon} alt="Dark mode" className="dark-mode__icon" />
         </button>
