884c58aae2626c7ea59577378f77f729156ce664
Adrian <66633552+Adrian-Capbatut@users.noreply.github.com>
2023-08-22 13:37:29 +0300

    Listening to changes in system settings with a theme

--- src/components/btnDarkMode/BtnDarkMode.js
+++ src/components/btnDarkMode/BtnDarkMode.js
@@ -1,41 +1,50 @@
 import { useEffect, useRef } from "react";
 import { useLocalStorage } from "../../utils/useLocalStorage";
 import detectDarkMode from "../../utils/detectDarkMode";
-import "./style.css"
+import "./style.css";
 
 import sun from "./sun.svg";
 import Moon from "./Moon.svg";
 
-
 const BtnDarkMode = () => {
-
-
-    const [darkMode, setDarkMode] = useLocalStorage('dark', detectDarkMode())
+    const [darkMode, setDarkMode] = useLocalStorage("dark", detectDarkMode());
     const btnRef = useRef(null);
 
-
     useEffect(() => {
-        if (darkMode === 'dark') {
-            document.body.classList.add('dark')
-            btnRef.current.classList.add('dark-mode-btn--active')
+        if (darkMode === "dark") {
+            document.body.classList.add("dark");
+            btnRef.current.classList.add("dark-mode-btn--active");
         } else {
-            document.body.classList.remove('dark')
-            btnRef.current.classList.remove('dark-mode-btn--active')
+            document.body.classList.remove("dark");
+            btnRef.current.classList.remove("dark-mode-btn--active");
         }
     }, [darkMode]);
 
+    useEffect(() => {
+        window
+            .matchMedia("(prefers-color-scheme: dark)")
+            .addEventListener("change", (event) => {
+                const newColorScheme = event.matches ? "dark" : "light";
+                setDarkMode(newColorScheme);
+            });
+    }, []);
+
     const toggleDarkMode = () => {
         setDarkMode((currentValue) => {
-            return currentValue === 'light' ? 'dark' : 'light'
-        })
-    }
+            return currentValue === "light" ? "dark" : "light";
+        });
+    };
 
     return (
-        <button ref={btnRef} button className="dark-mode-btn" onClick={toggleDarkMode} >
+        <button
+            ref={btnRef}
+            button
+            className="dark-mode-btn"
+            onClick={toggleDarkMode}>
             <img src={sun} alt="Light mode" className="dark-mode__icon" />
             <img src={Moon} alt="Dark mode" className="dark-mode__icon" />
-        </button >
+        </button>
     );
-}
+};
 
 export default BtnDarkMode;
